a9b561b292c83345e503e115da7dd5e755f9a46f
99002777 <72496952+99002777@users.noreply.github.com>
2020-10-20 09:11:07 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+import java.util.Comparator;
+
+
+class Main {
+  
+public static void main(String[] args) {
+
+ 
+
+ArrayList<String> languages = new ArrayList<>();
+
+    
+// add elements to ArrayList
+    
+var.add("Axis");
+    
+var.add("Kotak");
+    
+var.add("ICICI");
+    
+var.add("HDFC");
+    
+System.out.println("Unsorted ArrayList: " + var);
+
+    
+// sort the ArrayList in ascending order
+    
+var.sort(Comparator.naturalOrder());
+    
+System.out.println("Sorted ArrayList: " + var);
+  
+}
+
+}
